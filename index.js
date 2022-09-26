d1983ac423c5f7e3a9fc0d9023d47dee62c89973
ProProgrammer18 <omgharge18@gmail.com>
2022-09-27 00:43:55 +0530

    Added: Complete Design Change(Govt) + Login Page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require("http");
 const fs = require("fs");
+const express = require("express");
 
+const app = express();
+
+const login = fs.readFileSync(`${__dirname}/html/login.html`, "utf-8");
+const login_css = fs.readFileSync(`${__dirname}/css/login.css`, "utf-8");
 const home = fs.readFileSync(`${__dirname}/html/index.html`, "utf-8");
-const css = fs.readFileSync(`${__dirname}/css/style.css`, "utf-8");
+const home_css = fs.readFileSync(`${__dirname}/css/style.css`, "utf-8");
 const cardTemplate = fs.readFileSync(
   `${__dirname}/html/cardTemplate.html`,
   "utf-8"
@@ -15,28 +20,42 @@ function replaceTemplate(ele, temp) {
   temp = temp.replace(/{%IMGSRC%}/g, ele.img);
   temp = temp.replace(/{%CARDSUBTITLE%}/g, ele.subtitle);
   temp = temp.replace(/{%DESCRIPTION%}/g, ele.description);
-
+  temp = temp.replace(/{%DATE%}/g, ele.date);
+  temp = temp.replace(/{%NOTICEID%}/g, ele.notice_id);
+  temp = temp.replace(/{%DOWNLOAD%}/g, ele.download);
   return temp;
 }
 
 const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-type": "text/html",
-  });
-  let output = home.replace("/*STYLE*/", css);
+app.get("/", (req, res) => {
+  let output = login.replace("/*STYLE*/", login_css);
+  res.send(output);
+});
+
+app.get("/home", (req, res) => {
+  let output = home.replace("/*STYLE*/", home_css);
 
   let cardHtml = dataObj
     .map((ele) => replaceTemplate(ele, cardTemplate))
     .join("");
 
   output = output.replace("{%CARDS%}", cardHtml);
-  // console.log(output);
-  output = res.end(output);
+  res.send(output);
 });
 
-server.listen("80", () => {
-  console.log("Server running at port 80");
+app.post("/home", (req, res) => {
+  let output = home.replace("/*STYLE*/", home_css);
+
+  let cardHtml = dataObj
+    .map((ele) => replaceTemplate(ele, cardTemplate))
+    .join("");
+
+  output = output.replace("{%CARDS%}", cardHtml);
+  res.send(output);
+});
+
+app.listen("80", () => {
+  console.log("Server running at 80!");
 });
